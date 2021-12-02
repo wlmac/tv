ee0312dc6bdca56eda732264f647485d41056f73
Ken Shibata <kenxshibata@gmail.com>
2021-12-02 15:02:55 -0500

    prettify

--- js/announcements.js
+++ js/announcements.js
@@ -27,207 +27,234 @@ var idx = 0;
  */
 var a_pr;
 
-
 /**
  * Returns a club's name based on its ID
  * @param {number} id - Club ID
  * @returns {string} Club name
  */
 function getClub(id) {
-    for(var i = 0; i < clubs.length; i++) {
-        if(clubs[i].id == id) {
-            return clubs[i].name;
-        }
+  for (var i = 0; i < clubs.length; i++) {
+    if (clubs[i].id == id) {
+      return clubs[i].name;
     }
+  }
 }
 
 /**
  * Gets announcement list
  */
 function getAnnouncements() {
-    // Sets a new promise
-    a_pr = new Promise((resolve, reject) => {
-        try {
-            // Gets the club list from the API
-            $.getJSON("https://maclyonsden.com/api/organizations", function(orgs) {
-                if(orgs === undefined) {
-                    reject(); // Reject promise if nothing returned
-                }
-
-                clubs = orgs;
-
-                // Gets the announcement list from the API
-                $.getJSON("https://maclyonsden.com/api/announcements", function(announce) {
-                    if(announce === undefined) {
-                        reject(); // Reject promise if nothing returned
-                    }
-
-                    /** @type {number} */
-                    var index = 0;
-                    /**
-                     * Limits how old the announcements are
-                     * @type {Object}
-                     */
-                    var range = new Date();
-                    range.setDate(range.getDate()-5); // Sets range to 5 days ago
-
-                    // Find oldest announcement in range
-                    for(index = 0; index < announce.length; index++) {
-                        if(Date.parse(announce[index].last_modified_date) < range.getTime()) {
-                            break;
-                        }
-                    }
-
-                    // Sets new announcement list within range
-                    announcements = announce.slice(0, index+2);
-
-                    // Caps announcement list length at 8
-                    if(announcements.length > 8) {
-                        announcements = announcements.slice(0, 8);
-                    }
-
-                    console.log("API Call");
-                    resolve(); // Resolve promise if all succeeds
-                }).fail(() => {
-                    reject(); // Reject promise if API request fails
-                });
-            }).fail(() => {
-                reject(); // Reject promise if API request fails
-            });
-        } catch(err) {
-            console.log(err);
-            reject(); // Reject promise if error is thrown
+  // Sets a new promise
+  a_pr = new Promise((resolve, reject) => {
+    try {
+      // Gets the club list from the API
+      $.getJSON("https://maclyonsden.com/api/organizations", function (orgs) {
+        if (orgs === undefined) {
+          reject(); // Reject promise if nothing returned
         }
-    });
+
+        clubs = orgs;
+
+        // Gets the announcement list from the API
+        $.getJSON(
+          "https://maclyonsden.com/api/announcements",
+          function (announce) {
+            if (announce === undefined) {
+              reject(); // Reject promise if nothing returned
+            }
+
+            /** @type {number} */
+            var index = 0;
+            /**
+             * Limits how old the announcements are
+             * @type {Object}
+             */
+            var range = new Date();
+            range.setDate(range.getDate() - 5); // Sets range to 5 days ago
+
+            // Find oldest announcement in range
+            for (index = 0; index < announce.length; index++) {
+              if (
+                Date.parse(announce[index].last_modified_date) < range.getTime()
+              ) {
+                break;
+              }
+            }
+
+            // Sets new announcement list within range
+            announcements = announce.slice(0, index + 2);
+
+            // Caps announcement list length at 8
+            if (announcements.length > 8) {
+              announcements = announcements.slice(0, 8);
+            }
+
+            console.log("API Call");
+            resolve(); // Resolve promise if all succeeds
+          }
+        ).fail(() => {
+          reject(); // Reject promise if API request fails
+        });
+      }).fail(() => {
+        reject(); // Reject promise if API request fails
+      });
+    } catch (err) {
+      console.log(err);
+      reject(); // Reject promise if error is thrown
+    }
+  });
 }
 
 /**
  * Sets announcement
  */
 function setAnnouncement() {
-    try {
-        /**
-         * Current announcement data
-         * @type {Object}
-         */
-        var post = announcements[idx];
-        /**
-         * Announcement upload datetime
-         * @type {Object}
-         */
-        var upload = new Date(Date.parse(post.last_modified_date));
+  try {
+    /**
+     * Current announcement data
+     * @type {Object}
+     */
+    var post = announcements[idx];
+    /**
+     * Announcement upload datetime
+     * @type {Object}
+     */
+    var upload = new Date(Date.parse(post.last_modified_date));
 
-        // Separates upload information into variables
-        /** @type {string} */
-        var month = months[upload.getMonth()];
-        /** @type {number} */
-        var date = upload.getDate();
-        /** @type {number} */
-        var hr = upload.getHours();
-        /** @type {number} */
-        var min = upload.getMinutes();
-        /** @type {string} */
-        var period = "a.m.";
+    // Separates upload information into variables
+    /** @type {string} */
+    var month = months[upload.getMonth()];
+    /** @type {number} */
+    var date = upload.getDate();
+    /** @type {number} */
+    var hr = upload.getHours();
+    /** @type {number} */
+    var min = upload.getMinutes();
+    /** @type {string} */
+    var period = "a.m.";
 
-        // Next announcement
-        idx++;
-        idx %= announcements.length;
+    // Next announcement
+    idx++;
+    idx %= announcements.length;
 
-        // Time formatting
-        if(min < 10) {
-            min = "0"+min;
-        }
-        if(hr >= 12) {
-            if(hr > 12) {
-                hr -= 12;
-            }
-            period = "p.m.";
-        }
-        if(hr == 0) {
-            hr += 12
-        }
+    // Time formatting
+    if (min < 10) {
+      min = "0" + min;
+    }
+    if (hr >= 12) {
+      if (hr > 12) {
+        hr -= 12;
+      }
+      period = "p.m.";
+    }
+    if (hr == 0) {
+      hr += 12;
+    }
 
-        // Sets announcement data
-        $("#title").text(post.title);
-        $("#org").html(`${getClub(post.organization.id)}<date> • ${month} ${date}, ${hr}:${min} ${period}</date>`);
-        marked.setOptions({
-            breaks: true // Ensures linebreaks are rendered in markdown
-        });
-        $("#announcement-body").html(marked.parse(post.body.replaceAll("/media/", "https://maclyonsden.com/media/")));
+    // Sets announcement data
+    $("#title").text(post.title);
+    $("#org").html(
+      `${getClub(
+        post.organization.id
+      )}<date> • ${month} ${date}, ${hr}:${min} ${period}</date>`
+    );
+    marked.setOptions({
+      breaks: true, // Ensures linebreaks are rendered in markdown
+    });
+    $("#announcement-body").html(
+      marked.parse(
+        post.body.replaceAll("/media/", "https://maclyonsden.com/media/")
+      )
+    );
 
-        // Clears previous QR code
-        $("#qrinfo").empty();
-        $("#qrcode").empty();
-        // Sets QR code
-        $("#qrinfo").html("Scan to see<br>on site");
-        const qrcode = new QRCode(document.getElementById("qrcode"), {
-            text: `https://maclyonsden.com/announcement/${post.id}`,
-            width: 130,
-            height: 130,
-            colorDark : '#000',
-            colorLight : '#fff',
-            correctLevel : QRCode.CorrectLevel.H
-        });
-    } catch(err) {console.log(err); /* Log error */}
+    // Clears previous QR code
+    $("#qrinfo").empty();
+    $("#qrcode").empty();
+    // Sets QR code
+    $("#qrinfo").html("Scan to see<br>on site");
+    const qrcode = new QRCode(document.getElementById("qrcode"), {
+      text: `https://maclyonsden.com/announcement/${post.id}`,
+      width: 130,
+      height: 130,
+      colorDark: "#000",
+      colorLight: "#fff",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
+  } catch (err) {
+    console.log(err); /* Log error */
+  }
 }
 
 /**
  * Runs on page load
  */
-$(document).ready(function() {
-    getAnnouncements();
-    a_pr.then(() => {
-        // Sets the announcement once the promise is resolved
+$(document).ready(function () {
+  getAnnouncements();
+  a_pr
+    .then(() => {
+      // Sets the announcement once the promise is resolved
 
-        /**
-         * Time until next announcement, 3wpm + 5s * img
-         */
-        var t = Math.max(announcements[idx].body.split(" ").length*1000/3 + 5000*(announcements[idx].body.match(/\/media\//g) || []).length, 5000);
-        
-        execute();
+      /**
+       * Time until next announcement, 3wpm + 5s * img
+       */
+      var t = Math.max(
+        (announcements[idx].body.split(" ").length * 1000) / 3 +
+          5000 * (announcements[idx].body.match(/\/media\//g) || []).length,
+        5000
+      );
 
-        /**
-         * Calculates and sets new announcement timeout
-         */
-        function changeTimer() {
-            t = Math.max(announcements[idx].body.split(" ").length*1000/3 + 5000*(announcements[idx].body.match(/\/media\//g) || []).length, 5000);
+      execute();
+
+      /**
+       * Calculates and sets new announcement timeout
+       */
+      function changeTimer() {
+        t = Math.max(
+          (announcements[idx].body.split(" ").length * 1000) / 3 +
+            5000 * (announcements[idx].body.match(/\/media\//g) || []).length,
+          5000
+        );
+      }
+
+      /**
+       * Handles changing and timing of announcements
+       */
+      function execute() {
+        console.log(announcements[idx].title);
+        console.log(~~(t / 1000));
+
+        setAnnouncement();
+
+        // Get new announcement list after cycle
+        if (idx == 0) {
+          getAnnouncements();
         }
 
-        /**
-         * Handles changing and timing of announcements
-         */
-        function execute() {
-            console.log(announcements[idx].title);
-            console.log(~~(t/1000));
-
-            setAnnouncement();
-
-            // Get new announcement list after cycle
-            if(idx == 0) {
-                getAnnouncements();
-            }
-
-            /** @type {HTML Object} */
-            var scrollable = $("#announcement-body");
-            scrollable.scrollTop(0); // Sets scrollbar to top
-            if(t > 10000) {
-                // If announcement timeout is greater than 10s wait 5s before scrolling
-                setTimeout(function () {
-                    scrollable.animate({scrollTop: scrollable.prop("scrollHeight") }, t-10000);
-                }, 5000);
-            } else {
-                // Else scroll immediately
-                scrollable.animate({scrollTop: scrollable.prop("scrollHeight") }, t);
-            }
-
-            // Handles the variable timeout
-            changeTimer();
-            setTimeout(execute, t);
+        /** @type {HTML Object} */
+        var scrollable = $("#announcement-body");
+        scrollable.scrollTop(0); // Sets scrollbar to top
+        if (t > 10000) {
+          // If announcement timeout is greater than 10s wait 5s before scrolling
+          setTimeout(function () {
+            scrollable.animate(
+              { scrollTop: scrollable.prop("scrollHeight") },
+              t - 10000
+            );
+          }, 5000);
+        } else {
+          // Else scroll immediately
+          scrollable.animate({ scrollTop: scrollable.prop("scrollHeight") }, t);
         }
-    }).catch(() => {
-        // Sets error messages if promise is rejected
-        $("#title").text("Something went wrong :(");
-        $("#org").text("Please check back later");
-        $("#announcement-body").empty();
+
+        // Handles the variable timeout
+        changeTimer();
+        setTimeout(execute, t);
+      }
+    })
+    .catch(() => {
+      // Sets error messages if promise is rejected
+      $("#title").text("Something went wrong :(");
+      $("#org").text("Please check back later");
+      $("#announcement-body").empty();
     });
-})
+});
--- js/version.js
+++ js/version.js
@@ -6,38 +6,39 @@
  * @since       1.0.0
  */
 
-
 /**
  * Checks site version and updates
  */
 function updateVersion() {
-    var version;
-    var current = localStorage.getItem("site-version");
-    new Promise((resolve, reject) => {
-        try {
-            $.getJSON(`https://${location.host}/version.json`, function(ver) {
-                if(ver === undefined) {
-                    reject();
-                }
-                version = ver.version;
-                resolve();
-            }).fail(() => {
-                reject();
-            });
-        } catch(err) {
-            reject();
+  var version;
+  var current = localStorage.getItem("site-version");
+  new Promise((resolve, reject) => {
+    try {
+      $.getJSON(`https://${location.host}/version.json`, function (ver) {
+        if (ver === undefined) {
+          reject();
         }
-    }).then(() => {
-        if(version != current) {
-            localStorage.setItem("site-version", version);
-            location.reload();
-        }
-    }).catch(() => {})
+        version = ver.version;
+        resolve();
+      }).fail(() => {
+        reject();
+      });
+    } catch (err) {
+      reject();
+    }
+  })
+    .then(() => {
+      if (version != current) {
+        localStorage.setItem("site-version", version);
+        location.reload();
+      }
+    })
+    .catch(() => {});
 }
 
 /**
  * Runs on page load
  */
-$(document).ready(function() {
-    updateVersion();
-})
+$(document).ready(function () {
+  updateVersion();
+});
