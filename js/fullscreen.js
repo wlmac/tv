bbff3b66a92f3c27ed89c07ad039964357c52095
rcshim04 <rcshim04@gmail.com>
2021-12-05 00:10:39 -0500

    Moved fullscreen to function

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -3,26 +3,29 @@
  * @author      Eric Shim
  * @author      Ken Shibata
  * @author      Project Metropolis
- * @version     1.0.2
+ * @version     1.0.3
  * @since       1.0.0
  */
 
-
-$(document).ready(() => {
+function fullscreen() {
   /**
    * Gets the root element of the document
    * @type {HTML Object}
    */
   let elem = document.documentElement;
-  
+
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen(); // Chrome, Safari, iOS, Android
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen(); // Internet Explorer
+  }
+}
+
+$(document).ready(() => {
   // Fullscreens the page on button click
   $("#fullscreen-btn").click(() => {
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) {
-      elem.webkitRequestFullscreen(); // Chrome, Safari, iOS, Android
-    } else if (elem.msRequestFullscreen) {
-      elem.msRequestFullscreen(); // Internet Explorer
-    }
+    fullscreen();
   });  
 });
